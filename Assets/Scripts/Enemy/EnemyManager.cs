8b4b1f21c1cee03d952d636dbe91c276e36abc2a
stasheryn <98048913+stasheryn@users.noreply.github.com>
2024-09-22 14:37:03 +0300

    almost circle spawn enemys
    adjust min max values

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -7,7 +7,8 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] private float circleDistanceFromPlayer;
     [SerializeField] private float circleDistanceFromPlayerMax;
     [SerializeField] private Enemy _enemy;
-
+    [SerializeField] private Vector3 _playerCoordinates;
+    [SerializeField] private Transform player;
     [SerializeField] private EnemyHealthbar _hpBar;
 
     // parent for hold in CanvaWorldCoord object
@@ -22,8 +23,8 @@ public class EnemyManager : MonoBehaviour
     void Update()
     {
     }
-
-    public void SpawnEnemy()
+    // ПЕРЕРОБИТИ!
+    private Vector3 anotherCircleSpawn()
     {
         // проблема інстаюється енемі, і вже має бути інстаювання його хп бара,, як??
         Vector3 posToSpawn = new Vector3(2, 2, 0);
@@ -33,14 +34,47 @@ public class EnemyManager : MonoBehaviour
         float z = Mathf.Sin(0.5f) * circleDistanceFromPlayer;
         // transform треба брати у гравця
         Vector3 pos = transform.position + new Vector3(x, 0, z);
+        return pos;
+    }
+    
+    private Vector3 SpawnCircle()
+    {
+        // дивна реалізація, не впевнений що воно справді спавнить по кругу, краще спробувати замінити на метод з cos+sin
+        float x, z;
+        // X or Z axis
+        bool sideToSpawn = Random.value > 0.5f;
+
+        if (sideToSpawn)
+        {
+            x = Random.Range(circleDistanceFromPlayerMax, circleDistanceFromPlayerMax);
+            z = Random.value > 0.5f
+                ? Random.Range(-circleDistanceFromPlayerMax, -circleDistanceFromPlayer)
+                : Random.Range(circleDistanceFromPlayer, circleDistanceFromPlayerMax);
+        }
+        else
+        {
+            x = Random.value > 0.5f
+                ? Random.Range(-circleDistanceFromPlayerMax, -circleDistanceFromPlayer)
+                : Random.Range(circleDistanceFromPlayer, circleDistanceFromPlayerMax);
+            z = Random.Range(circleDistanceFromPlayerMax, circleDistanceFromPlayerMax);
+        }
+        // не факт що там круг, прямокутник?
+        Vector3 randCirclePos = player.position + new Vector3(x, 0, z);
+        return randCirclePos;
+    }
+
+    public void SpawnEnemy()
+    {
+        // переробити один з них
+        var posToSpawn = SpawnCircle();
 
         // спитати якщо-що за спавн в ієрархії інспектора
         var enemyDoll = Instantiate(_enemy, posToSpawn, Quaternion.identity, parentWhereInstate);
-        
+
         // створити метод який асайнить префаб у скрипт, викликати цей метод тут після інстаншуювання першого
         //Instantiate(_hpBar)
         //_enemy.hpBar=_hpBar;
-        
+
         // виправити парента
         var enemyDollHP = Instantiate(_hpBar, posToSpawn, Quaternion.identity, parentHP);
         enemyDoll.AddReference(enemyDollHP);
